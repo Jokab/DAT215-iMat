f5e58adc068b8d7dccc967db8ffc531faf8f4453
Sebastian Blomberg <sebastianblomberg@hotmail.com>
2013-02-25 13:07:51 +0100

    Added a main controller and a viewcontroller interface

---
+++ src/MainController.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+/**
+ * The main controller of the application
+ * @author Sebastian Blomberg
+ *
+ */
+public class MainController {
+	private LinkedList<ViewController> history = new LinkedList<ViewController>();
+	
+	public MainController() {
+		
+	}
+	
+	public void switchController(ViewController controller) {
+		
+	}
+}
---
+++ src/ViewController.java
@@ -0,0 +1,8 @@
+/**
+ * An interface to be implemented by all view-controllers,
+ * to be able to store history.
+ * @author Sebastian Blomberg
+ *
+ */
+public interface ViewController {
+}
