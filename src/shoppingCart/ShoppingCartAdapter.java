68de971e67b1e95e595ef2783d9c46a4e3750db1
Sebastian Blomberg <sebastianblomberg@hotmail.com>
2013-02-27 12:03:05 +0100

    Forece changes

--- src/shoppingCart/ShoppingCartAdapter.java
+++ src/shoppingCart/ShoppingCartAdapter.java
@@ -1,4 +1,4 @@
-package shoppingcart;
+package shoppingCart;
 
 import java.util.Iterator;
 import java.util.List;
--- src/shoppingcart/ShoppingCartProductPanel.java
+++ src/shoppingcart/ShoppingCartProductPanel.java
@@ -1,4 +1,4 @@
-package shoppingcart;
+package shoppingCart;
 
 import javax.swing.JPanel;
 
