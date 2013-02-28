96b65719c6f04a1d48cd6d53fb60229bb791eea5
Sebastian Blomberg <sebastianblomberg@hotmail.com>
2013-02-28 09:38:11 +0100

    Set background of productsList to white

--- src/productView/ProductListView.java
+++ src/productView/ProductListView.java
@@ -21,14 +21,15 @@ public class ProductListView extends JPanel {
 		setOpaque(false);
 		setBorder(null);
 		setLayout(new GridLayout(0, 1, 0, 0));
+		scrollPane.setBorder(null);
 		scrollPane.setOpaque(false);
+		scrollPane.setBackground(Color.WHITE);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBackground(Color.LIGHT_GRAY);
 		
 		add(scrollPane);
+		viewPanel.setBackground(Color.WHITE);
 		viewPanel.setBorder(null);
-		viewPanel.setOpaque(false);
 		
 		scrollPane.setViewportView(viewPanel);
 		viewPanel.setLayout(new GridLayout(0, 1, 0, 0));
